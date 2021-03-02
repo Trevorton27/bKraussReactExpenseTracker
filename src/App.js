9d214432f9b124616ca13d72a4795ee9f5dd0603
BryanGK <bryangordonkrauss@gmail.com>
2021-03-02 13:11:28 -0800

    Moved logic to App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,41 @@ import Footer from './components/Footer';
 import Table from './components/Table';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: Math.random(),
+            date: "",
+            location: "",
+            amount: "",
+            description: "",
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        const savedExpenses = JSON.parse(localStorage.getItem('expenseArray')) || [];
+        const newExpense = this.state;
+        savedExpenses.push(newExpense);
+        localStorage.setItem('expenseArray', JSON.stringify(savedExpenses));
+        this.setState();
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render() {
         return (
             <div>
                 <Header />
-                <Input />
+                <Input
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                />
                 <Table />
                 <Footer />
             </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,77 +1,47 @@
 import React from 'react';
 
-class Input extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            id: Math.random(),
-            date: "",
-            location: "",
-            amount: "",
-            description: "",
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+function Input(props) {
 
-    handleSubmit(event) {
-        event.preventDefault();
-        const savedExpenses = JSON.parse(localStorage.getItem('expenseArray')) || [];
-        const newExpense = this.state;
-        savedExpenses.push(newExpense);
-        localStorage.setItem('expenseArray', JSON.stringify(savedExpenses));
-        this.setState();
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    render() {
-
-        return (
-            <div>
-                <form className="input-field" onSubmit={this.handleSubmit}>
-                    <input
-                        type="date"
-                        name="date"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        name="location"
-                        placeholder="Enter expense location"
-                        onChange={this.handleChange}
-                    />
+    return (
+        <div>
+            <form className="input-field" onSubmit={props.handleSubmit}>
+                <input
+                    type="date"
+                    name="date"
+                    onChange={props.handleChange}
+                />
+                <input
+                    type="text"
+                    name="location"
+                    placeholder="Enter expense location"
+                    onChange={props.handleChange}
+                />
 
 
-                    <input
-                        type="number"
-                        name="amount"
-                        placeholder="Enter an amount"
-                        step="0.01"
-                        onChange={this.handleChange}
-                    />
+                <input
+                    type="number"
+                    name="amount"
+                    placeholder="Enter an amount"
+                    step="0.01"
+                    onChange={props.handleChange}
+                />
 
 
-                    <input
-                        type="text"
-                        name="description"
-                        placeholder="Expense description"
-                        onChange={this.handleChange}
-                    />
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Expense description"
+                    onChange={props.handleChange}
+                />
 
 
-                    <button
-                        type="submit">
-                        Add to list
+                <button
+                    type="submit">
+                    Add to list
                     </button>
-                </form>
-            </div>
-        )
-    }
+            </form>
+        </div>
+    )
 }
 
 export default Input
